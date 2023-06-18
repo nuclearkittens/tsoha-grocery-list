4d1f96279c22cf9c3de0e45ba7e13b82b2e27e07
meri saynatkari <meri.saynatkari@gmail.com>
2023-06-18 15:06:42 +0300

    update error handling

--- src/routes.py
+++ src/routes.py
@@ -6,7 +6,7 @@ import users
 
 # @app.before_request
 # def before_request():
-#     authorised = ['index', 'login', 'register']
+#     authorised = ['index', 'login', 'login/check', 'register', 'register/check']
 #     endpoint = request.endpoint
 #     user_id = users.get_user_id()
 
@@ -17,17 +17,27 @@ import users
 
 #     return
 
+@app.errorhandler(404)
+def page_not_found(e):
+    message = '404_page_not_found'
+    return redirect(url_for('error', message=message))
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    message = '405_method_not_allowed'
+    return redirect(url_for('error', message=message))
+
 @app.route('/')
 def index():
     '''Render the main page.'''
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
     '''Render the login page'''
     return render_template('login.html')
 
-@app.route('/login_check', methods=['POST'])
+@app.route('/login/check', methods=['POST'])
 def login_check():
     '''Check if user is logged in.'''
     username = request.form['username']
@@ -36,7 +46,7 @@ def login_check():
     logged_in, err = users.login(username, password)
     if not logged_in:
         msg = ', '.join(err)
-        return redirect(url_for('error', message=msg, prev='login'))
+        return redirect(url_for('error', message=msg))
 
     return redirect(url_for('login'))
 
@@ -45,24 +55,18 @@ def error():
     '''Render the error page with either a specified or
     general error message.'''
     message = request.args.get('message')
-    prev = request.args.get('prev')
 
     if not message:
         message = 'unidentified_error'
 
-    if not prev:
-        prev = url_for('index')
-    else:
-        prev = url_for(prev)
-
-    return render_template('error.html', message=message, prev=prev)
+    return render_template('error.html', message=message)
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     '''Render the logout page.'''
     return render_template('logout.html')
 
-@app.route('/logout_check', methods=['POST'])
+@app.route('/logout/check', methods=['POST'])
 def logout_check():
     '''Log user out.'''
     users.logout()
@@ -73,7 +77,7 @@ def register():
     '''Render the registration page or related errors.'''
     return render_template('register.html')
 
-@app.route('/register_check', methods=['POST'])
+@app.route('/register/check', methods=['POST'])
 def register_check():
     '''Check the registration.'''
     username = request.form['username']
@@ -82,14 +86,14 @@ def register_check():
     print(username, password, password_check)
     if password != password_check:
         return redirect(url_for(
-            'error', message='password_does_not_match', prev='register'
+            'error', message='password_does_not_match'
         ))
 
     registered, err = users.register(username, password)
     print(registered, err)
     if not registered:
         msg = ', '.join(err)
-        return redirect(url_for('error', message=msg, prev='register'))
+        return redirect(url_for('error', message=msg))
 
     return redirect(url_for('register'))
 
